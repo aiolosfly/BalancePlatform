9016a4a0ab22fead86f62b346dd58bb880098cab
aiolosfly <1656219949@qq.com>
2016-07-01 20:53:07 +0800

    ok commit draw_structure

--- Draw_structure/Draw_structure.ino
+++ Draw_structure/Draw_structure.ino
@@ -4,49 +4,52 @@
 int value = 0;
 String buffer = "";
 
+double angle_rad = PI/180.0;
+double angle_deg = 180.0/PI;
 
 MePort port(4);
 
 Servo servo1;//inside
 Servo servo2;//outside
 
-const float L1=13.0;//armLength,inside
-const float L2=10.0;//armLength,outside
-const int accuracy = 20;
+const double L1=13.0;//armLength,inside
+const double L2=10.0;//armLength,outside
+const int accuracy = 10;
 
 struct Angle
 {
-  float angleVal1;//inside
-  float angleVal2; //outside
-  };
+  double angleVal1;//inside
+  double angleVal2; //outside
+};
 
 struct Point
 {
-  float xVal;
-  float yVal;
-  
-  };
+  double xVal;
+  double yVal;
+};
 void moveTopoint(Angle);
-Angle pointToangle(float,float);
-Point angleTopoint(float,float);
-Angle angles[50];
+Angle pointToangle(double,double);
+Point angleTopoint(double,double);
+Angle angles[20];
 void drawLine(Point startpos,Point endpos);
-void drawCircle(Point centre,float radius);
+void drawCircle(Point centre,double radius);
+
+void setpoint(String x,String y);
 
 void runAngle();
 
 void parseBuffer();
 
 long lastTime = 0; 
-int runTime = 0;
+long runTime = 0;
 int len = 0;
 Angle currentAngle;
 Angle targetAngle;
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(115200);
     Serial.println("order:/pos/x/y/");
     Serial.println("please input the point(x,y):");
-    
+        
     servo1.attach(port.pin1());
     servo2.attach(port.pin2());     
     
@@ -55,19 +58,12 @@ void setup() {
     currentAngle.angleVal1 = 90;
     currentAngle.angleVal2 = 90;
   
-//     Point startpos,endpos;
-//    startpos = {-5,20};
-//    endpos = {5,20};
-//    drawLine(startpos,endpos);
-
-  //Circle
-  Point centre = {0,20};
-  float radius = 2;
-  drawCircle(centre,radius);
-
- 
-
-
+    delay(2000);
+    setpoint(2,20);
+    setpoint(-2,20);  
+    setpoint(-2,16);
+    setpoint(2,16);
+    setpoint(2,20);
 
 }
 
@@ -109,14 +105,14 @@ void parseBuffer(){
   }
   
   if(values[0].equals("pos")){
-    setpoint(values[1],values[2]);
+    setpoint(values[1].toFloat(),values[2].toFloat());
   }
   
   Serial.println(buffer);
   buffer = "";
 }
 
-void setpoint(String x,String y)
+void setpoint(float x,float y)
 {
   Serial.println();
   Serial.print("Move to the point:");
@@ -126,14 +122,9 @@ void setpoint(String x,String y)
   Serial.print(y);
   Serial.println(")");
   
-  Point pos;
-  Angle angle;
-  pos.xVal = x.toFloat();
-  pos.yVal = y.toFloat();
-  angle = pointToangle(pos.xVal,pos.yVal);
+  Angle angle = pointToangle(x,y);
   pushTopoint(angle);
-    
-  }
+}
 
 void moveTopoint(Angle angle)
 {
@@ -141,17 +132,28 @@ void moveTopoint(Angle angle)
 }
 void runAngle(){
     if(micros()-lastTime>runTime){
-      int dA1=(targetAngle.angleVal1-currentAngle.angleVal1);
-      dA1 = dA1>0?-1:(dA1<0?1:0);
-      int dA2=(targetAngle.angleVal2-currentAngle.angleVal2);
-      dA2 = dA2>0?-1:(dA2<0?1:0);
+      float dA1=(targetAngle.angleVal1-currentAngle.angleVal1);
+      float dA2=(targetAngle.angleVal2-currentAngle.angleVal2);
+      if(abs(dA1)>abs(dA2)){
+        dA1 = dA1>0.5?(-abs(dA1)/abs(dA2)):(dA1<-0.5?(abs(dA1)/abs(dA2)):0);
+        dA2 = dA2>0.5?-1:(dA2<-0.5?1:0);
+      }else if(abs(dA1)<abs(dA2)){
+        dA2 = dA2>0.5?(-abs(dA2)/abs(dA1)):(dA2<-0.5?(abs(dA2)/abs(dA1)):0);
+        dA1 = dA1>0.5?-1:(dA1<-0.5?1:0);
+      }else{
+        dA1 = dA1>0.5?-1:(dA1<-0.5?1:0);
+        dA2 = dA2>0.5?-1:(dA2<-0.5?1:0);
+      }
+      
       currentAngle.angleVal1 -= dA1;
       currentAngle.angleVal2 -= dA2;
       servo1.write(currentAngle.angleVal1);
       servo2.write(currentAngle.angleVal2);
       lastTime = micros();
-      runTime = max(abs(dA1),abs(dA2))*20000;
-      if(dA1==0&&dA2==0){
+      runTime = 45000;
+      if(dA1==0||dA2==0){
+        currentAngle.angleVal1 = targetAngle.angleVal1;
+        currentAngle.angleVal2 = targetAngle.angleVal2;
         callNextAngle();
       }
     }
@@ -169,41 +171,41 @@ void pushTopoint(Angle angle){
   angles[len] = angle;
   len++;
 }
-Angle pointToangle(float x,float y)
+float rads = 180/PI;
+Angle pointToangle(double x,double y)
 {
     Angle angle;
-  
-    angle.angleVal2=acos((x*x+y*y-L1*L1-L2*L2)/(2*L1*L2))*(180/3.14)+90;
-    angle.angleVal1=atan2(y,x)*(180/3.14)-atan2(L2*sin((angle.angleVal2-90)*(3.14/180)),(L1+L2*cos((angle.angleVal2-90)*(3.14/180))))*(180/3.14);
-
-//    if(angle.angleVal1>180||angle.angleVal2<0||angle.angleVal2>180||angle.angleVal2<0){
-//      Serial.println("Can not draw!");
-//      angle = {90,90};  //回到起点 
-//      }
+    
+    angle.angleVal2=acos((x*x+y*y-L1*L1-L2*L2)/(2*L1*L2))*(rads)+90;
+    angle.angleVal1=(atan2(y,x)-atan2(L2*sin((angle.angleVal2-90)/(rads)),(L1+L2*cos((angle.angleVal2-90)/rads))))*(rads);
     return angle;
   }
 
- Point angleTopoint(float angleVal1,float angleVal2)
+ Point angleTopoint(double angleVal1,double angleVal2)
  {
   Point point;
   
-  point.xVal = L1*cos(angleVal1*(3.14/180))+L2*cos(angleVal1*(3.14/180)+angleVal2*(3.14/180));
-  point.yVal = L1*sin(angleVal1*(3.14/180))+L2*sin(angleVal1*(3.14/180)+angleVal2*(3.14/180));
+  point.xVal = L1*cos(angleVal1*(3.14/180))+L2*cos(angleVal1*(3.14/180)+angleVal2*(3.14/180)-90*(3.14/180));
+  point.yVal = L1*sin(angleVal1*(3.14/180))+L2*sin(angleVal1*(3.14/180)+angleVal2*(3.14/180)-90*(3.14/180));
   
   return point;
   }
 
 void drawLine(Point startpos,Point endpos)
 {
-  float k = (endpos.yVal-startpos.yVal)/(endpos.xVal-startpos.xVal); //斜率
   Angle angle;
+  Point pos=startpos;
   angle = pointToangle(startpos.xVal,startpos.yVal);  
   pushTopoint(angle);
-  Point pos=startpos;
+  
+  if(endpos.xVal!=startpos.xVal){
+  double k = (endpos.yVal-startpos.yVal)/(endpos.xVal-startpos.xVal); //斜率
+  
   for(int i=1;i<=10;i++)
   {
     pos.xVal=pos.xVal+(endpos.xVal-startpos.xVal)/10;              //取点
     pos.yVal=k*(pos.xVal-startpos.xVal)+startpos.yVal;   //直线方程
+    
     Serial.println("===========point===============");    
     Serial.println(pos.xVal);
     Serial.println(pos.yVal);
@@ -218,8 +220,25 @@ void drawLine(Point startpos,Point endpos)
     
     pushTopoint(angle);         //移动
     }  
+  }
+  else{
+  for(int i=1;i<15;i++)//垂直的情况
+    {
+      pos.xVal=startpos.xVal+pow(-1,(double)i)*0.2;
+      pos.yVal=pos.yVal+(endpos.yVal-startpos.yVal)/20;
+      Serial.println("===========point===============");    
+      Serial.println(pos.xVal);
+      Serial.println(pos.yVal);
+      angle=pointToangle(pos.xVal,pos.yVal);
+      Serial.println("===========angle===============");    
+      Serial.println(angle.angleVal1);
+      Serial.println(angle.angleVal2);
+      pushTopoint(angle);
+      
+      }
+  }
 }
-void drawCircle(Point centre,float radius)
+void drawCircle(Point centre,double radius)
 {
   Angle angle;
   Point pos;
@@ -235,66 +254,30 @@ void drawCircle(Point centre,float radius)
     Serial.println("radius is Wrong!");
     
     }
-  
-  pos={centre.xVal+radius,centre.yVal}; 
-    Serial.println("===========point_start===============");    
-    Serial.println(pos.xVal);
-    Serial.println(pos.yVal);
-  angle = pointToangle(pos.xVal,pos.yVal);
-    Serial.println("===========angle_start===============");    
-    Serial.println(angle.angleVal1);
-    Serial.println(angle.angleVal2);
-   pushTopoint(angle);
 
-    for(int i=0 ;i<accuracy;i++)
+    for(int i=0;i<=360;i=i+360/accuracy)
     {
-    pos.xVal=pos.xVal+(-2*radius)/(accuracy);
-//    Serial.println("");
-//    Serial.println((pos.xVal-2*radius)/(accuracy-1));
-    pos.yVal=sqrt(radius*radius-(pos.xVal-centre.xVal)*(pos.xVal-centre.xVal))+centre.yVal;
+      pos.xVal=centre.xVal+radius*cos(i*angle_rad);
+      pos.yVal=centre.yVal+radius*sin(i*angle_rad);
 
-   
-    Serial.println("===========point1===============");    
-    Serial.println(pos.xVal);
-    Serial.println(pos.yVal);
-   
-    angle = pointToangle(pos.xVal,pos.yVal);          //位置转换为角度
+      Serial.println("==============circle=============");
+      Serial.println((pos.xVal-centre.xVal)*(pos.xVal-centre.xVal)+(pos.yVal-centre.yVal)*(pos.yVal-centre.yVal));
+      Serial.println(radius*radius);
 
-    Serial.println("===========angle1===============");    
-    Serial.println(angle.angleVal1);
-    Serial.println(angle.angleVal2);
-//    
-    pushTopoint(angle);
-  }
+      Serial.println("==============circle_angle=============");
+      Serial.println(i);
+      Serial.println(i*angle_rad);
 
-  //转接点
-//  pos={centre.xVal-radius,centre.yVal}; 
-    Serial.println("===========point_mid===============");    
-    Serial.println(pos.xVal);
-    Serial.println(pos.yVal);
-//  angle = pointToangle(pos.xVal,pos.yVal);
-    Serial.println("===========angle_mid===============");    
-    Serial.println(angle.angleVal1);
-    Serial.println(angle.angleVal2);
-  // pushTopoint(angle);
-  
-  //另一半圆
-    for(int i=0 ;i<accuracy;i++)
-    {
-    pos.xVal=pos.xVal+(2*radius)/(accuracy);
-    pos.yVal= -sqrt(radius*radius-(pos.xVal-centre.xVal)*(pos.xVal-centre.xVal))+centre.yVal;
+      Serial.println("==============point===============");
+      Serial.println(pos.xVal);
+      Serial.println(pos.yVal);
 
+      angle=pointToangle(pos.xVal,pos.yVal);
+      Serial.println("==============angle===============");
+      Serial.println(angle.angleVal1);
+      Serial.println(angle.angleVal2);      
+      pushTopoint(angle);
+      
+    }
     
-    Serial.println("===========point2===============");    
-    Serial.println(pos.xVal);
-    Serial.println(pos.yVal);
-    
-    angle = pointToangle(pos.xVal,pos.yVal);          //位置转换为角度
-    Serial.println("===========angle2===============");    
-    Serial.println(angle.angleVal1);
-    Serial.println(angle.angleVal2);
-    
-   pushTopoint(angle);  
-  }
-   
   }
